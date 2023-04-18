6b53c5f774217ae817427d0047a8c37430361ed8
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-18 09:37:08 +0100

    Update the setup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'assets/scss/styles.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from 'redux/store';
+import store from 'app/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
