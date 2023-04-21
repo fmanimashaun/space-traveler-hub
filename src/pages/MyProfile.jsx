9d32126079b68364f457df9ca10075791c24b894
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-21 10:19:42 +0100

    Update the layout in profile component

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Stack } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Reserved from 'components/Reserved';
 
 const MyProfile = () => {
@@ -10,20 +10,26 @@ const MyProfile = () => {
 
   return (
     <>
-      <Stack gap={5} direction="horizontal">
-        <Reserved
-          title="Rockets"
-          arr={reservedRockets}
-          idKey="id"
-          nameKey="name"
-        />
-        <Reserved
-          title="Missions"
-          arr={reservedMissions}
-          idKey="mission_id"
-          nameKey="mission_name"
-        />
-      </Stack>
+      <Container>
+        <Row>
+          <Col lg={6}>
+            <Reserved
+              title="Rockets"
+              arr={reservedRockets}
+              idKey="id"
+              nameKey="name"
+            />
+          </Col>
+          <Col lg={6}>
+            <Reserved
+              title="Missions"
+              arr={reservedMissions}
+              idKey="mission_id"
+              nameKey="mission_name"
+            />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
