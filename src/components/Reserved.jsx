6d02a6341e80cf7af30bb1298aec20daa6618ebd
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-23 15:50:53 +0100

    Update the readme file

--- src/components/Reserved.jsx
+++ src/components/Reserved.jsx
@@ -2,29 +2,30 @@ import Table from 'react-bootstrap/Table';
 import PropTypes from 'prop-types';
 import Styles from 'assets/scss/reservedTable.module.scss';
 
-const Reserved = ({
-  title,
-  arr,
-}) => (
-  <div className="flex-fill">
-    <h2 className="h2">{`My ${title}`}</h2>
-    <div className={Styles.roundedborder}>
-      {arr.length > 0 ? (
-        <Table className="mb-0">
-          <tbody>
-            {arr.map((item) => (
-              <tr key={item.id}>
-                <td className="pt-3 pb-4 ps-3 pe-3">{item.name}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      ) : (
-        <p className="text-center pt-3 pb-4 ps-3 pe-3 mb-0">{`No reserved ${title}`}</p>
-      )}
+const Reserved = ({ title, arr }) => {
+  const titleName = title === 'Rockets' ? 'rocket' : 'mission';
+
+  return (
+    <div className="flex-fill">
+      <h2 className={titleName}>{`My ${title}`}</h2>
+      <div className={Styles.roundedborder}>
+        {arr.length > 0 ? (
+          <Table className="mb-0">
+            <tbody>
+              {arr.map((item) => (
+                <tr key={item.id}>
+                  <td className="pt-3 pb-4 ps-3 pe-3">{item.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        ) : (
+          <p className="text-center pt-3 pb-4 ps-3 pe-3 mb-0">{`No reserved ${title}`}</p>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Reserved.propTypes = {
   title: PropTypes.string.isRequired,
